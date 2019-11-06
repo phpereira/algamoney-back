4193e439bd7a040953fe791d696d17d134bc6487
Paulo Henrique Pereira <phpereira@outlook.com>
2019-11-06 19:26:21 -0300

    Criação das models Pessoa e Endereco

---
+++ algamoney-api/src/main/java/com/exemple/algamoney/api/model/Pessoa.java
@@ -0,0 +1,70 @@
+package com.exemple.algamoney.api.model;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.Objects;
+
+@Entity
+@Table(name = "pessoa")
+public class Pessoa {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long codigo;
+
+    @NotNull
+    @Size(min = 10, max = 50)
+    private String nome;
+
+    @NotNull
+    private Boolean ativo;
+
+    @Embedded
+    private Endereco endereco;
+
+    public Long getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(Long codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Boolean getAtivo() {
+        return ativo;
+    }
+
+    public void setAtivo(Boolean ativo) {
+        this.ativo = ativo;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Pessoa)) return false;
+        Pessoa pessoa = (Pessoa) o;
+        return getCodigo().equals(pessoa.getCodigo());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getCodigo());
+    }
+}
